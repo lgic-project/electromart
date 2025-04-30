46bd5acf157181919229e0c35237361842699c17
Prabhat Raj Poudel <poudelprabhat625@gmail.com>
2025-04-30 22:13:17 +0545

    including css styles for logo and onboarding screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,14 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { StatusBar, View, StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+import OnBoarding from './(routes)/onboarding/index'; // ✅ Corrected Import
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -27,7 +17,6 @@ export default function RootLayout() {
     ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
@@ -46,14 +35,26 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        {isLoggedIn ? (
+          <View />
+        ) : (
+          <>
+            <OnBoarding />
+            <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+          </>
+        )}
+      </View>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+});
--- screens/onboarding/onboarding.screen.tsx
+++ screens/onboarding/onboarding.screen.tsx
@@ -1,8 +1,8 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity,StyleSheet } from "react-native";
 import { useFonts, Raleway_700Bold } from "@expo-google-fonts/raleway";
 import { Nunito_400Regular, Nunito_700Bold } from "@expo-google-fonts/nunito";
 import { LinearGradient } from "expo-linear-gradient";
-
+import { styles } from "@/styles/onboarding/onboard";
 import { router } from "expo-router";
 
 
@@ -16,8 +16,18 @@ export default function OnBoardingScreen() {
         return null;
       }
   return (
-    <View>
-      <Text>onboardingscreen</Text>
+    <LinearGradient
+    colors={["#E5ECF9", "#F6F7F9"]}
+    style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+  >
+    <View style={styles.firstContainer}>
+      <View>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+        
+      </View>
+     
+     
     </View>
+  </LinearGradient>
   )
 }

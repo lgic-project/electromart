39b5ba96cd0c39c0e48fc00cf1ad0ff17840b323
KapiL Tiwari <kapiltiwari2025@gmail.com>
2025-06-17 21:59:09 +0545

    Adding css to not_found file

--- src/app/+not_found.tsx
+++ src/app/+not_found.tsx
@@ -11,3 +11,10 @@ export default function NotFoundScreen() {
     </>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

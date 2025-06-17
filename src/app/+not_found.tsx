18ac121d51ae761467932d32b4aaef27659e8e15
KapiL Tiwari <kapiltiwari2025@gmail.com>
2025-06-17 21:50:59 +0545

    Add import

--- src/app/+not_found.tsx
+++ src/app/+not_found.tsx
@@ -1,1 +1,13 @@
-import { Link, Stack } from 'expo-router';
+mport { Link, Stack } from 'expo-router';
+import { View, StyleSheet } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops! This screen doesn't exist." }} />
+      <View style={styles.container}>
+        <Link href="/">Go to home screen</Link>
+      </View>
+    </>
+  );
+}

86dd026e0d61a69fa3f0b3d0ff34281d897c8e8a
KapiL Tiwari <kapiltiwari2025@gmail.com>
2025-06-19 09:37:48 +0545

    updating const components for cart file

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -13,3 +13,17 @@ import { useState } from 'react';
 import { useCartStore } from '../../store/cart-store';
 import { getProduct } from '../../api/api';
 import { ActivityIndicator } from 'react-native';
+
+const ProductDetails = () => {
+  const { slug } = useLocalSearchParams<{ slug: string }>();
+  const toast = useToast();
+
+  const { data: product, error, isLoading } = getProduct(slug);
+
+  const { items, addItem, incrementItem, decrementItem } = useCartStore();
+
+  const cartItem = items.find(item => item.id === product?.id);
+
+  const initialQuantity = cartItem ? cartItem.quantity : 0;
+
+  const [quantity, setQuantity] = useState(initialQuantity);

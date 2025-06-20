eb873cb4a5fc1a9d66820d5bd4194e32a2e48074
Prabhat Raj Poudel <poudelprabhat625@gmail.com>
2025-06-20 22:43:26 +0545

    fetchhing products and categories api

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,3 +3,17 @@ import { supabase } from '../lib/supabase';
 import { useAuth } from '../providers/auth-provider';
 import { generateOrderSlug } from '../utils/utils';
 
+export const getProductsAndCategories = () => {
+  return useQuery({
+    queryKey: ['products', 'categories'],
+    queryFn: async () => {
+      const [products, categories] = await Promise.all([
+        supabase.from('product').select('*'),
+        supabase.from('categories').select('*'),
+      ]);
+
+     
+    },
+  });
+};
+

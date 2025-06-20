f8a1066c76ba435176712d6408ac8da83d47a126
Prabhat Raj Poudel <poudelprabhat625@gmail.com>
2025-06-20 23:04:22 +0545

    removed errors and unused loc

--- src/api/subscriptions.ts
+++ src/api/subscriptions.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 
 import { supabase } from '../lib/supabase';
-import {act} from 'react';
+
 
 export const useOrderUpdateSubscription = () => {
   const queryClient = useQueryClient();
@@ -17,7 +17,6 @@ export const useOrderUpdateSubscription = () => {
           console.log('Change received!', payload);
           queryClient.invalidateQueries({
             queryKey: ['orders'],
-            .queryKey: ['order', payload.new.id],
           });
         }
       )

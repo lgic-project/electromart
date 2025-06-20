104f71c1ad959bc14064ec9871bd7a1defd90ac5
Prabhat Raj Poudel <poudelprabhat625@gmail.com>
2025-06-20 22:35:22 +0545

    importing datafetching and statemanagement functions from @tanstack library

---
+++ src/api/api.ts
@@ -0,0 +1,5 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { supabase } from '../lib/supabase';
+import { useAuth } from '../providers/auth-provider';
+import { generateOrderSlug } from '../utils/utils';
+
